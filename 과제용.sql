8f94b7f06c59d8e1606312816b2870909562c8a4
skclsso25 <110643104+Rhosungchan@users.noreply.github.com>
2022-09-06 17:50:39 +0900

    20220906

--- 과제용.sql
+++ 과제용.sql
@@ -156,6 +156,16 @@ WHERE SUBSTR(PROFESSOR_SSN, 8,1) = '1'
 ORDER BY 나이;
 
 -------------------------------------------------
+-- Additional SELECT 4번) 0
+-- 교수들의 이름 중 성을 제외한 이름만 출력하는 SQL 문장을 작성하시오. 
+-- 출력 헤더는 "이름" 이 찍히도록 한다. 
+--(성이 2자인 경우 교수는 없다고 가정하시오)
+
+
+SELECT SUBSTR(PROFESSOR_NAME,2,2) 
+FROM TB_PROFESSOR 
+
+-------------------------------------------------
 -- Additional SELECT 5번)
 /* 춘 기술 대학교의 재수생 입학자를 구하려고 한다. 
  * 어떻게 찾아낼 것인가? 
@@ -167,22 +177,89 @@ WHERE EXTRACT(YEAR FROM ENTRANCE_DATE) -
   	  EXTRACT(YEAR FROM TO_DATE(SUBSTR(STUDENT_SSN,1,6))) > 19;
   	 
 -------------------------------------------------
--- Additional SELECT 10번)
+-- Additional SELECT 6번)	 0
+-- 2020년 크리스마스는 무슨 요일인가?
+   	 
+ SELECT TO_CHAR(TO_DATE('2020-12-25'),'DAY') FROM DUAL; 	 
+  	 
+-------------------------------------------------
+-- Additional SELECT 7번)	 0
+-- TO_DATE('99/10/11', 'YY/MM/DD'), TO_DATE('49/10/11', 'YY/MM/DD')은
+-- 각각 몇 년 몇 월 몇 일을 의미할까? 
+-- 또 TO_DATE('99/10/11', 'RR/MM/DD'), TO_DATE('49/10/11', 'RR/MM/DD')은
+-- 각각 몇 년 몇 월 몇 일을 의미할까?
+
+SELECT TO_DATE('99/10/11', 'YY/MM/DD') FROM DUAL; 
+SELECT TO_DATE('49/10/11', 'YY/MM/DD') FROM DUAL; 
+SELECT TO_DATE('99/10/11', 'RR/MM/DD') FROM DUAL; 
+SELECT TO_DATE('49/10/11', 'RR/MM/DD') FROM DUAL; 
+  	 
+-------------------------------------------------
+-- Additional SELECT 8번)	 0
+-- 춘 기술대학교의 2000년도 이후 입학자들은 학번이 A로 시작하게 되어있다. 
+-- 2000년도 이전 학번을 받은 학생들의 학번과 이름을 보여주는 SQL 문장을 작성하시오.
+
+SELECT STUDENT_NO , STUDENT_NAME 
+FROM TB_STUDENT
+--WHERE ENTRANCE_DATE < '2000-01-01'; -- 풀이 1번
+WHERE STUDENT_NO NOT LIKE 'A%';       -- 풀이 2번
+  	 
+-------------------------------------------------
+-- Additional SELECT 9번)	 0
+-- 학번이 A517178 인 한아름 학생의 학점 총 평점을 구하는 SQL 문을 작성하시오. 
+-- 단, 이때 출력 화면의 헤더는 "평점" 이라고 찍히게 하고, 
+-- 점수는 반올림하여 소수점 이하 한 자리까지만 표시한다.
+
+SELECT ROUND(AVG(POINT),1) 평점
+FROM TB_GRADE 
+WHERE STUDENT_NO = 'A517178';
+
+
+
+-------------------------------------------------
+-- Additional SELECT 10번)   0
 -- 학과 별 학생 수를 구하여 "학과 번호", "학생수(명)"의 형태로 
 -- 헤더를 만들어 결과값이 출력되도록하시오.
 
+
+-- 내 풀이 
+SELECT DEPARTMENT_NO "학과 번호" , COUNT(*) "학생수(명)" 
+FROM TB_STUDENT 
+GROUP BY DEPARTMENT_NO; 
+
+
+
+-- 강사 풀이
 SELECT DEPARTMENT_NO "학과 번호" ,COUNT(*) "학생수(명)"
 FROM TB_STUDENT 
 GROUP BY DEPARTMENT_NO
 ORDER BY 1;
 
 -------------------------------------------------
--- Additional SELECT 12번)
+-- Additional SELECT 11번)   0
+/* 지도 교수를 배정받지 못한 학생의 수는 몇 명 정도 되는지 알아내는 SQL문을 작성하시오
+*/
+
+SELECT COUNT(*)  
+FROM TB_STUDENT 
+WHERE COACH_PROFESSOR_NO IS NULL; 
+
+-------------------------------------------------
+-- Additional SELECT 12번) 0 
 /* 학번이 A112113인 김고운 학생의 년도 별 평점을 구하는 SQL 문을 작성하시오
  * 단, 이때 출력 화면의 헤더는 "년도", "년도 별 평점"이라고 찍히게 하고,
  * 점수는 반올림하여 소수점 이하 한자리까지만 표시한다.
  */
 
+-- 내 풀이 
+SELECT SUBSTR(TERM_NO,1,4)년도 , ROUND(AVG(POINT),1)  
+FROM TB_GRADE 
+WHERE STUDENT_NO = 'A112113'
+GROUP BY SUBSTR(TERM_NO,1,4)
+ORDER BY 1;
+
+
+-- 강사풀이 
 SELECT SUBSTR(TERM_NO,1,4 ) 년도, ROUND( AVG(POINT),1)"년도 별 평점" 
 FROM TB_GRADE
 WHERE STUDENT_NO = 'A112113'
@@ -190,9 +267,16 @@ GROUP BY SUBSTR(TERM_NO,1,4 )
 ORDER BY 년도;
 
 -------------------------------------------------
--- Additional SELECT 13번)
+-- Additional SELECT 13번) 0 
 /* 학과 별 휴학생 수를 파악하고자 한다. 
  * 학과 번호와 휴학생 수를 표시하는  SQL문장을 작성하시오 */
+
+-- 내 풀이 
+
+SELECT DEPARTMENT_NO 학과 , COUNT(DECODE(ABSENCE_YN,'Y',1)) "휴학생 수" 
+FROM TB_STUDENT 
+GROUP BY DEPARTMENT_NO; 
+
 
 --문제 풀이 1번 SUM을 활용
 SELECT DEPARTMENT_NO "학과 코드명",
@@ -209,10 +293,18 @@ GROUP BY DEPARTMENT_NO
 ORDER BY DEPARTMENT_NO;
 
 -------------------------------------------------
--- Additional SELECT 14번)
+-- Additional SELECT 14번) 0
 -- 춘 대학교에 다니는 동명이인 학생들의 이름을 찾고자 한다
 -- 어떤 SQL문장을 사용하면 가능하겠는가?
 
+-- 내 풀이 
+SELECT STUDENT_NAME  , COUNT(*) 
+FROM TB_STUDENT 
+HAVING COUNT(*) > 1 
+GROUP BY STUDENT_NAME;
+
+
+--강사님 풀이
 SELECT STUDENT_NAME , COUNT(*) "동명인 수"
 FROM TB_STUDENT
 GROUP BY STUDENT_NAME
@@ -226,6 +318,19 @@ ORDER BY 1;
  * 단, 이때 출력 화면의 헤더는 "년도", "년도 별 평점"이라고 찍히게 하고, 
  * 점수는 반올림하여 소수점 이하 한 자리까지만 표시한다. */
 
+
+-- 내 풀이
+
+SELECT NVL(SUBSTR(TERM_NO,1,4),' ')년도,
+       NVL(SUBSTR(TERM_NO,5,2),' ')학기, 
+       ROUND(AVG(POINT),1) 평점
+FROM TB_GRADE  
+WHERE STUDENT_NO ='A112113'
+GROUP BY ROLLUP (SUBSTR(TERM_NO,1,4),SUBSTR(TERM_NO,5,2));
+
+
+
+-- 강사 풀이 
 SELECT NVL(SUBSTR(TERM_NO,1,4), ' ')년도,
 	   NVL(SUBSTR(TERM_NO,5,2),' ')학기,
 	   ROUND(AVG(POINT),1) 평점
